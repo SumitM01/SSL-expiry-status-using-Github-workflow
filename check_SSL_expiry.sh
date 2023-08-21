224c09aaaf5de0887f1f37e5caa3b6db5bcb020f
SumitM01 <cr7sumitmishra@gmail.com>
2023-08-21 13:29:41 +0530

    create ssl_expiry_check.sh and domains.txt file

---
+++ check_SSL_expiry.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# send_slack_notification() {
+#     local webhook_url="$1"
+#     local message="$2"
+#     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$webhook_url"
+# }
+
+get_ssl_expiry_date() {
+    local domain="$1"
+    echo | openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -d= -f2
+}
+
+while read -r domain; do
+    expiry_date=$(get_ssl_expiry_date "$domain")
+    expiry_date_seconds=$(date -d"$expiry_date" +%s)
+    current_date_seconds=$(date +%s)
+    remaining_days=$(( (expiry_date_seconds - current_date_seconds) / 86400 ))
+    if (( remaining_days < 30 )); then
+        message="SSL Expiry Alert\n* Domain : $domain\n* Warning : The SSL certificate for $domain will expire in $remaining_days days."
+        # send_slack_notification "$SLACK_WEBHOOK_URL" "$message"
+    fi
+done < domains.txt
