01fdc82ac28ed980dcb5c56cf75746743a7b594b
Sumit Mishra <cr7sumitmishra@gmail.com>
2023-08-23 11:30:45 +0530

    Update check_SSL_expiry.sh

--- check_SSL_expiry.sh
+++ check_SSL_expiry.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-# send_slack_notification() {
-#     local webhook_url="$1"
-#     local message="$2"
-#     curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$webhook_url"
-# }
+send_slack_notification() {
+    local webhook_url="$1"
+    local message="$2"
+    curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$message\"}" "$webhook_url"
+}
 
 get_ssl_expiry_date() {
     local domain="$1"
     echo | openssl s_client -servername "$domain" -connect "$domain":443 2>/dev/null | openssl x509 -noout -dates | grep notAfter | cut -d= -f2
 }
+
+send_slack_notiication "$SLACK_WEBHOOK_URL" "Aug 21 - Task-1"
 
 while read -r domain; do
     expiry_date=$(get_ssl_expiry_date "$domain")
@@ -18,5 +20,5 @@ while read -r domain; do
     remaining_days=$(( (expiry_date_seconds - current_date_seconds) / 86400 ))
     message="SSL Expiry Alert\n* Domain : $domain\n* Warning : The SSL certificate for $domain will expire in $remaining_days days."
     echo $message
-    # send_slack_notification "$SLACK_WEBHOOK_URL" "$message"
+    send_slack_notification "$SLACK_WEBHOOK_URL" "$message"
 done < domains.txt
